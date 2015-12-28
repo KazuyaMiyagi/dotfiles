ac9263cef7211a38a825f454a7f2a7d017973f17
Kazuya MIYAGI <umi.kukki@gmail.com>
2015-12-28 11:38:07 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+DOTFILE_DIR=$(cd $(dirname $0) && pwd)
+
+curl --fail --location --create-dirs --output ${DOTFILE_DIR}/vim/autoload/plug.vim \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln --symbolic --no-dereference $DOTFILE_DIR/vim/ ~/.vim
+ln --symbolic $DOTFILE_DIR/tmux.conf ~/.tmux.conf
+ln --symbolic $DOTFILE_DIR/gitconfig ~/.gitconfig
+ln --symbolic $DOTFILE_DIR/gitignore ~/.gitignore
+ln --symbolic $DOTFILE_DIR/gemrc ~/.gemrc
+ln --symbolic --no-dereference $DOTFILE_DIR/bundle/ ~/.bundle
+
+echo "source $DOTFILE_DIR/bashrc" >> ~/.bashrc
+echo "source $DOTFILE_DIR/zshrc" >> ~/.zshrc
