2c895f43a59e6eee1172aec384261921e1e8408d
Kazuya MIYAGI <umi.kukki@gmail.com>
2015-12-28 14:11:34 +0900

    Update git-update.sh

--- git-update.sh
+++ git-update.sh
@@ -4,17 +4,17 @@ if [ ! $(id -u) -eq 0 ] ; then
 fi
 
 if [ -f /etc/redhat-release ] ; then
-yum -y install git curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel
-pushd /usr/local/src
-if [ ! -d git ] ; then
-    git clone git://git.kernel.org/pub/scm/git/git.git
-fi
-cd git
-git pull
-make configure
-./configure --prefix=/usr/local
-make all
-make install
-make clean
-popd
+    yum -y install git curl-devel expat-devel gettext-devel openssl-devel perl-devel zlib-devel
+    pushd /usr/local/src
+    if [ ! -d git ] ; then
+        git clone --depth 1 git://git.kernel.org/pub/scm/git/git.git
+    fi
+    cd git
+    git pull
+    make configure
+    ./configure --prefix=/usr/local
+    make all
+    make install
+    make clean
+    popd
 fi
