3e2ec51e43d012901980a34de3e3b289631920d9
Kazuya MIYAGI <kazuya@Haruya-air.local>
2016-07-06 23:48:12 +0900

    Update

--- init.sh
+++ init.sh
@@ -4,13 +4,13 @@ DOTFILE_DIR=$(cd $(dirname $0) && pwd)
 curl --fail --location --create-dirs --output ${DOTFILE_DIR}/vim/autoload/plug.vim \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-ln --symbolic --no-dereference $DOTFILE_DIR/vim/ ~/.vim
-ln --symbolic $DOTFILE_DIR/tmux.conf ~/.tmux.conf
-ln --symbolic $DOTFILE_DIR/gitconfig ~/.gitconfig
-ln --symbolic $DOTFILE_DIR/gitignore ~/.gitignore
-ln --symbolic $DOTFILE_DIR/gemrc ~/.gemrc
-ln --symbolic $DOTFILE_DIR/my.cnf ~/.my.cnf
-ln --symbolic --no-dereference $DOTFILE_DIR/bundle/ ~/.bundle
+ln -s -n $DOTFILE_DIR/vim/      ~/.vim
+ln -s -n $DOTFILE_DIR/bundle/   ~/.bundle
+ln -s    $DOTFILE_DIR/gemrc     ~/.gemrc
+ln -s    $DOTFILE_DIR/my.cnf    ~/.my.cnf
+ln -s    $DOTFILE_DIR/tmux.conf ~/.tmux.conf
+ln -s    $DOTFILE_DIR/gitconfig ~/.gitconfig
+ln -s    $DOTFILE_DIR/gitignore ~/.gitignore
 
 echo "source $DOTFILE_DIR/bashrc" >> ~/.bashrc
 echo "source $DOTFILE_DIR/zshrc" >> ~/.zshrc
