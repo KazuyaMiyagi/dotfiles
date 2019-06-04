67b2c2145e2990726d723e3782389b3691b0b955
Kazuya MIYAGI <umi.kukki@gmail.com>
2019-06-04 10:07:44 +0900

    Add zsh

--- zsh/autoload/prompt.zsh
+++ zsh/autoload/prompt.zsh
@@ -1,35 +1,20 @@
-export LESSCHARSET=utf-8
-export PATH=/usr/local/sbin:~/bin:~/.composer/vendor/bin:$PATH
-p() { peco | while read LINE; do $@ $LINE; done }
-alias o='git ls-files | p open'
-alias j='ghq list -p | p cd'
-alias be='bundle exec'
-eval "$(direnv hook zsh)"
-eval "$(rbenv init -)"
-export PATH="$GOPATH/bin:$PATH"
-export GOPATH="$HOME/.go"
-export PATH="$HOME/bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$HOME/.nodenv/shims:$PATH"
-eval "$(nodenv init -)"
-
-alias phpunit-coverage='phpunit --coverage-html reports --coverage-clover reports/coverage.xml'
-alias tree='tree -N -C'
-export DOCKER_BUILDKIT=1
-alias tf='terraform'
-
 autoload -Uz add-zsh-hook
 autoload -Uz vcs_info
 
-GIT_STAGED_SYMBOL='✔︎'
-GIT_UNSTAGED_SYMBOL='+'
-GIT_UNTRACKED_SYMBOL='?'
+# https://nerdfonts.com/
+readonly AWS_SYMBOL='%F{214} %f'
+readonly GIT_SYMBOL='%F{202} %f'
+readonly GIT_STAGED_SYMBOL='✔︎'
+readonly GIT_UNSTAGED_SYMBOL='+'
+readonly GIT_UNTRACKED_SYMBOL='?'
+
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' check-for-staged-changes true
 zstyle ':vcs_info:*' stagedstr "${GIT_STAGED_SYMBOL}"
 zstyle ':vcs_info:*' unstagedstr "${GIT_UNSTAGED_SYMBOL}"
 zstyle ':vcs_info:*' formats "%s(%b) %u%c"
 zstyle ':vcs_info:*' actionformats "%s(%b|%a) %u%c"
+
 _vcs_info_precmd() {
    vcs_info
    psvar=()
@@ -37,13 +22,14 @@ _vcs_info_precmd() {
    [[ -n $vcs_info_msg_1_ ]] && psvar[2]=$vcs_info_msg_1_
    [[ -n $AWS_PROFILE ]] && psvar[3]=$AWS_PROFILE
 }
+
 add-zsh-hook precmd _vcs_info_precmd
 
-#https://github.com/zsh-users/zsh/blob/f702e17b14d75aa21bff014168fa9048124db286/Misc/vcs_info-examples#L155-L170
+# https://github.com/zsh-users/zsh/blob/f702e17b14d75aa21bff014168fa9048124db286/Misc/vcs_info-examples#L155-L170
 
 ### Display the existence of files not yet known to VCS
 
-### git: Show marker (T) if there are untracked files in repository
+### git: Show marker (${GIT_UNTRACKED_SYMBOL}) if there are untracked files in repository
 # Make sure you have added staged to your 'formats':  %c
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
@@ -57,9 +43,8 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
         hook_com[staged]+="${GIT_UNTRACKED_SYMBOL}"
     fi
 }
-GIT_SYMBOL='%F{202} %f'
-AWS_SYMBOL='%F{214} %f'
+
 export PROMPT="
-[%n%F{magenta}@%f%M %F{cyan}%c%f] %1(v|%B${GIT_SYMBOL}%1v%b|) %2(v|%B%F{yellow}%2v%f%b|) %3(v|%B${AWS_SYMBOL}AWS_PROFILE(%3v%)%b|) %(?..%B%F{red}EXIT_CODE(%?%)%f%b)
+[%n@%m %c] %1(v|${GIT_SYMBOL}%1v|) %2(v|%2v|) %3(v|${AWS_SYMBOL}AWS_PROFILE(%3v%)|) %(?..%F{red}EXIT_CODE(%?%)%f)
 %(?.%F{green}.%F{red})%(#.#.$)%f "
 export RPROMPT=
---
+++ zsh/autoload/xdg.zsh
@@ -0,0 +1,9 @@
+# https://wiki.archlinux.jp/index.php/XDG_Base_Directory_%E3%82%B5%E3%83%9D%E3%83%BC%E3%83%88
+#
+# XDG Settings
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+
+# zsh
+export HISTFILE=$XDG_DATA_HOME/zsh/history
