09839e8e7967d693fcd1bda4587ea0f234e8673e
Kazuya MIYAGI <umi.kukki@gmail.com>
2019-06-09 00:17:59 +0900

    Add zplug

---
+++ zsh/autoload/zplug.zsh
@@ -0,0 +1,18 @@
+export ZPLUG_HOME=/usr/local/opt/zplug
+
+if [ -f $ZPLUG_HOME/init.zsh ]; then
+    source $ZPLUG_HOME/init.zsh
+
+    zplug "mollifier/anyframe"
+    if zplug check "mollifier/anyframe"; then
+        bindkey '^r' anyframe-widget-put-history
+        bindkey '^j' anyframe-widget-cd-ghq-repository
+    fi
+
+    zplug "zsh-users/zsh-autosuggestions"
+    zplug "zsh-users/zsh-syntax-highlighting"
+
+    zplug "kinjo/geed", as:command, use:"geed-*"
+
+    zplug load
+fi
