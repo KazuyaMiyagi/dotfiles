69b86a47bef2ec08747c742cbb109edfe58e69a9
Kazuya Miyagi <umi.kukki@gmail.com>
2020-06-30 20:09:37 +0900

    Update GHQ_ROOT

--- zsh/autoload/xdg.zsh
+++ zsh/autoload/xdg.zsh
@@ -1,15 +1,18 @@
 # https://wiki.archlinux.jp/index.php/XDG_Base_Directory_%E3%82%B5%E3%83%9D%E3%83%BC%E3%83%88
 
 # XDG Settings
-export XDG_CONFIG_HOME=$HOME/.config
-export XDG_CACHE_HOME=$HOME/.cache
-export XDG_DATA_HOME=$HOME/.local/share
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
 
 # zsh
-export HISTFILE=$XDG_DATA_HOME/zsh/history
+export HISTFILE="$XDG_DATA_HOME/zsh/history"
 
 # go
 export GOPATH="$XDG_DATA_HOME/go"
+
+# ghq
+export GHQ_ROOT="$XDG_DATA_HOME/go/src"
 
 # RubyGems
 export GEM_HOME="$XDG_DATA_HOME/gem"
@@ -22,4 +25,4 @@ export COMPOSER_BIN_DIR="$XDG_DATA_HOME/composer/vendor/bin"
 alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
 
 # tig
-export TIGRC_USER="$XDG_CONFIG_HOME"/tig/tigrc
+export TIGRC_USER="$XDG_CONFIG_HOME/tig/tigrc"
