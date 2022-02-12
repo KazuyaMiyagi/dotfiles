e7283a402783e499c2f98149d9b2a50bc5ffd0a1
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2022-02-12 21:38:43 +0900

    Use asdf-vm

--- zsh/autoload/xdg.zsh
+++ zsh/autoload/xdg.zsh
@@ -23,3 +23,8 @@ alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
 
 # tig
 export TIGRC_USER="$XDG_CONFIG_HOME/tig/tigrc"
+
+# asdf
+export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${XDG_CONFIG_HOME}/tool-versions"
+export ASDF_DATA_DIR="${XDG_DATA_HOME}/asdf"
