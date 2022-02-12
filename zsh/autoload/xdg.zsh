cc5cdb91a5cf044583a59b2c05941edf8f97ca91
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2022-02-12 21:58:15 +0900

    Update Homebrew update script

--- zsh/autoload/xdg.zsh
+++ zsh/autoload/xdg.zsh
@@ -1,28 +1,28 @@
 # https://wiki.archlinux.jp/index.php/XDG_Base_Directory_%E3%82%B5%E3%83%9D%E3%83%BC%E3%83%88
 
 # XDG Settings
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
 
 # zsh
-export HISTFILE="$XDG_DATA_HOME/zsh/history"
+export HISTFILE="${XDG_DATA_HOME}/zsh/history"
 
 # go
-export GOPATH="$XDG_DATA_HOME/git"
+export GOPATH="${XDG_DATA_HOME}/git"
 
 # ghq
-export GHQ_ROOT="$XDG_DATA_HOME/git/src"
+export GHQ_ROOT="${XDG_DATA_HOME}/git/src"
 
 # RubyGems
-export GEM_HOME="$XDG_DATA_HOME/gem"
-export GEM_SPEC_CACHE="$XDG_CACHE_HOME/gem"
+export GEM_HOME="${XDG_DATA_HOME}/gem"
+export GEM_SPEC_CACHE="${XDG_CACHE_HOME}/gem"
 
 # tmux
-alias tmux="tmux -f $XDG_CONFIG_HOME/tmux/tmux.conf"
+alias tmux="tmux -f ${XDG_CONFIG_HOME}/tmux/tmux.conf"
 
 # tig
-export TIGRC_USER="$XDG_CONFIG_HOME/tig/tigrc"
+export TIGRC_USER="${XDG_CONFIG_HOME}/tig/tigrc"
 
 # asdf
 export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
