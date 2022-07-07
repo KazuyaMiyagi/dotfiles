6a10139038b37cd92c31527376cf4ab3b53fd973
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2022-07-08 00:35:53 +0900

    Use terraform arm64 arch

--- zsh/autoload/zzz-asdf.zsh
+++ zsh/autoload/zzz-asdf.zsh
@@ -1,5 +1,4 @@
 . ${HOMEBREW_PREFIX}/opt/asdf/asdf.sh
  eval "$(asdf exec direnv hook zsh)"
 
-export ASDF_HASHICORP_OVERWRITE_ARCH_TERRAFORM="amd64"
 export POWERLINE_REPOSITORY_ROOT=$(asdf exec pip show powerline-status | grep Location | cut -d' ' -f 2)
