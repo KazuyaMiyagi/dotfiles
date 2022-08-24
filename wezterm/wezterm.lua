917a0689f3571f301aa4e3513c0d6aa605ff845f
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2022-08-24 14:37:41 +0900

    Fix font settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -43,11 +43,11 @@ return {
     initial_rows = 48,
 
     font_size = 14,
-    font = wezterm.font_with_fallback { "Hack Nerd Font" },
+    font = wezterm.font_with_fallback({ "Hack Nerd Font" }),
 
     color_scheme = "tokyonight",
     window_frame = {
-        font = wezterm.font { family = "Hack Nerd Font", weight = 'Bold' },
+        font = wezterm.font({ family = "Hack Nerd Font", weight = 'Bold' }),
         font_size = 14,
         active_titlebar_bg = '#333333',
         inactive_titlebar_bg = '#333333',
