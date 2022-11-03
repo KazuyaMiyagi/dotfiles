e803a0f225ccfd2219c51cbe54d9260f71c467f8
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2022-11-03 15:38:02 +0900

    Use Ruby 3.1.2

--- zsh/autoload/000-homebrew.zsh
+++ zsh/autoload/000-homebrew.zsh
@@ -5,4 +5,11 @@ if [[ "$(/usr/bin/uname)" == "Darwin" ]] ; then
         export HOMEBREW_PREFIX="/usr/local"
     fi
     eval "$(${HOMEBREW_PREFIX}/bin/brew shellenv)"
+
+    # for ruby-build
+    # https://github.com/rbenv/ruby-build/wiki#macos
+    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@3)"
+    # https://github.com/rbenv/ruby-build/discussions/1933#discussioncomment-2131144
+    export CPPFLAGS="-I$(brew --prefix)/opt/capstone/include"
+    export LDFLAGS="-L$(brew --prefix)/opt/capstone/lib"
 fi
