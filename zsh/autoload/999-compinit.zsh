e9b93756d842cbb20cba404914f9c0d23275a2f0
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2022-11-03 16:00:49 +0900

    Add bindkey reverse-menu-complete on Shift-Tab

--- zsh/autoload/999-compinit.zsh
+++ zsh/autoload/999-compinit.zsh
@@ -3,4 +3,7 @@ zstyle ":completion:*" menu select
 zstyle ":completion:*" matcher-list "m:{a-z}={A-Za-z}"
 zstyle ":completion:*" format "%B%d%b"
 
+# [Shift-Tab]
+bindkey "${terminfo[kcbt]}" reverse-menu-complete
+
 eval "$(kubectl completion zsh)"
