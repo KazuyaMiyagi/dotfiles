a63ab5fe23f74590f32b0b18b47d56d17e2880d4
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2023-12-08 12:04:32 +0900

    Add TF_PLUGIN_CACHE_DIR

--- zsh/autoload/200-export.zsh
+++ zsh/autoload/200-export.zsh
@@ -4,3 +4,4 @@ export LESSCHARSET=utf-8
 export TF_CLI_ARGS_plan="--parallelism=50"
 export TF_CLI_ARGS_apply="--parallelism=50"
 export TF_CLI_ARGS_refresh="--parallelism=50"
+export TF_PLUGIN_CACHE_DIR="${XDG_CACHE_HOME}/terraform/plugin-cache"
