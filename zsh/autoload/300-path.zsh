5803526541aaafc356a1a3623fe2d28fa1793646
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2023-12-11 10:09:34 +0900

    PATH を修正

--- zsh/autoload/300-path.zsh
+++ zsh/autoload/300-path.zsh
@@ -1,11 +1,11 @@
 path=(
+    ${XDG_CONFIG_HOME}/bin(N-/)
     ${HOMEBREW_PREFIX}/bin(N-/)
     ${HOMEBREW_PREFIX}/sbin(N-/)
     ${HOMEBREW_PREFIX}/opt/curl/bin(N-/)
     ${HOMEBREW_PREFIX}/opt/openssl@3/bin(N-/)
     /Applications/draw.io.app/Contents/MacOS(N-/)
     ${GOPATH}/bin(N-/)
-    ${XDG_CONFIG_HOME}/bin(N-/)
     ${path}
 )
 
