8aadfc00a4fff54c141d5a16e0c142ddf4dc35ba
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2025-11-12 17:08:01 +0900

    feat: update path

--- zsh/autoload/300-path.zsh
+++ zsh/autoload/300-path.zsh
@@ -6,6 +6,7 @@ path=(
     ${HOMEBREW_PREFIX}/sbin(N-/)
     ${HOMEBREW_PREFIX}/opt/curl/bin(N-/)
     ${HOMEBREW_PREFIX}/opt/openssl@3/bin(N-/)
+    ${HOMEBREW_PREFIX}/opt/libpq/bin(N-/)
     /Applications/draw.io.app/Contents/MacOS(N-/)
     /Applications/Amazon\ Q.app/Contents/MacOS(N-/)
     ${GOPATH}/bin(N-/)
