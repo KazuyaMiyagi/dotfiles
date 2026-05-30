94d5ae674732abaa575996ea3d9b5597446f4090
Kazuya Miyagi <KazuyaMiyagi@users.noreply.github.com>
2026-05-30 19:40:37 +0900

    feat: add Takumi Guard config for Ruby

--- zsh/autoload/100-xdg.zsh
+++ zsh/autoload/100-xdg.zsh
@@ -18,9 +18,9 @@ export GHQ_ROOT="${XDG_DATA_HOME}/git/src"
 export GEM_SPEC_CACHE="${XDG_CACHE_HOME}/gem"
 
 # Ruby#Bundler
-export BUNDLE_USER_CONFIG="${XDG_CONFIG_HOME}/bundle"
-export BUNDLE_USER_CACHE="${XDG_CACHE_HOME}/bundle"
-export BUNDLE_USER_PLUGIN="${XDG_DATA_HOME}/bundle"
+export BUNDLE_USER_CONFIG="${XDG_CONFIG_HOME}/bundle/config"
+export BUNDLE_USER_CACHE="${XDG_CACHE_HOME}/bundle/cache"
+export BUNDLE_USER_PLUGIN="${XDG_DATA_HOME}/bundle/plugin"
 
 # npm
 export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
